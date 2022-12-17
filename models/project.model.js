de44ac61a9eb55f2aeb6b1725e8a441c075f4dfd
Vedant Borkar <vedantborkar1234@gmail.com>
2022-12-18 02:28:43 +0530

    Update project.model.js

--- models/project.model.js
+++ models/project.model.js
@@ -1,9 +1,19 @@
 import { Schema, model } from 'mongoose';
 
-const User = require('./user');
-const Form = require('./form');
+const user = require('./user');
+const form = require('./form');
 
-const projectSchema = new Schema({name: { type: String, required: true}, id: { type: String, required: true, default: 'abcdefghijklmnop'}, owner: {type: Schema.Types.ObjectId, ref: 'User'}, collaborators: {type: [Schema.Types.ObjectId], ref: 'User'}, forms: {type: [Schema.Types.ObjectId], ref: 'Form'}, allowedOrigins: [{type: String}], reCaptchaKey: String, reCaptchaSecret: String}, { timestamps: true });
+function generate(len) {
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  let result = ' ';
+  const charactersLength = characters.length;
+  for(let i = 0; i < len; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const projectSchema = new Schema({name: { type: String, required: true}, id: { type: String, required: true, default: {generate(16)}}, owner: {type: Schema.Types.ObjectId, ref: 'user'}, collaborators: {type: [Schema.Types.ObjectId], ref: 'user'}, forms: {type: [Schema.Types.ObjectId], ref: 'form'}, allowedOrigins: [{type: String}], reCaptchaKey: String, reCaptchaSecret: String}, { timestamps: true });
 
 const Project = model('project', projectSchema);
 
