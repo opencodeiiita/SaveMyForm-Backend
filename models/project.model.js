bc8442dc71ed666417f0d1ba8e83ebd10370d31e
Vedant Borkar <vedantborkar1234@gmail.com>
2022-12-18 02:07:23 +0530

    Update project.model.js

--- models/project.model.js
+++ models/project.model.js
@@ -3,7 +3,7 @@ import { Schema, model } from 'mongoose';
 const User = require('./user');
 const Form = require('./form');
 
-const projectSchema = new Schema({name: { type: String, required: true}, id: { type: String, required: true, default: 'guitarist'}, owner: {type: Schema.Types.ObjectId, ref: 'User'}, collaborators: {type: [Schema.Types.ObjectId], ref: 'User'}, forms: {type: [Schema.Types.ObjectId], ref: 'Form'}, allowedOrigins: [{type: String}], reCaptchaKey: String, reCaptchaSecret: String}, { timestamps: true });
+const projectSchema = new Schema({name: { type: String, required: true}, id: { type: String, required: true, default: 'abcdefghijklmnop'}, owner: {type: Schema.Types.ObjectId, ref: 'User'}, collaborators: {type: [Schema.Types.ObjectId], ref: 'User'}, forms: {type: [Schema.Types.ObjectId], ref: 'Form'}, allowedOrigins: [{type: String}], reCaptchaKey: String, reCaptchaSecret: String}, { timestamps: true });
 
 const Project = model('project', projectSchema);
 
