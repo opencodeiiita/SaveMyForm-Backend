b6e2390cdfab0444ce28f5115b3c7d267b22d212
Vedant Borkar <vedantborkar1234@gmail.com>
2022-12-18 01:11:49 +0530

    Update project.model.js

--- models/project.model.js
+++ models/project.model.js
@@ -1,6 +1,8 @@
 import { Schema, model } from 'mongoose';
 
-const projectSchema = new Schema({}, { timestamps: true });
+const User = require('./user');
+
+const projectSchema = new Schema({name: String, id: String, owner: {type: [Schema.Types.ObjectId], ref: 'User'}, collaborators: [], forms: [], allowedOrigins: [], reCaptchaKey: String, reCaptchaSecret: String}, { timestamps: true });
 
 const Project = model('project', projectSchema);
 
