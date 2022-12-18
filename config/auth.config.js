7b20f974b5a1dced6c9c10768169f6d5b6251973
Anurag Jain <76420937+BuddyLongLegs@users.noreply.github.com>
2022-12-18 17:24:30 +0530

    Update auth.config.js

--- config/auth.config.js
+++ config/auth.config.js
@@ -3,4 +3,19 @@ import LocalStrategy from 'passport-local';
 import GoogleStrategy from 'passport-google-oauth20';
 import { Strategy as JWTStrategy } from 'passport-jwt';
 
+// Strategy function for local signin authentication
+// paasport.use('user-signin-local',);
+
+// Strategy function for local signup authentication
+// paasport.use('user-signup-local',);
+
+// Strategy function for Google OAuth2.0 authentication
+// paasport.use('google-auth',);
+
+// Strategy function for Google OAuth2.0 authentication
+// paasport.use('google-auth',);
+
+// Strategy function to verify JWT token recieved in request
+// passport.use('jwt',);
+
 export default passport;
