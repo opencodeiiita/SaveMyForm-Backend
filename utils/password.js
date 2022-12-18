d436f33ecbde9d4273279ca5270561c8cc3b45c9
Vedant Borkar <vedantborkar1234@gmail.com>
2022-12-18 19:27:11 +0530

    Create JWT

--- utils/password.js
+++ utils/password.js
@@ -3,7 +3,35 @@ import dotev from 'dotenv';
 
 dotev.config()
 
-export function getJwt(object){}
+function base64UrlEncode(str) {
+  // Encode the string as base64
+  const base64 = btoa(str);
+
+  // Replace characters that are not URL-safe with their URL-safe counterparts
+  return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
+
+export function getJwt(object){
+  // Set the algorithm for signing the JWT
+  const algorithm = 'HS256';
+
+  // Set the JWT header
+  const header = {
+    alg: algorithm,
+    typ: 'JWT'
+  };
+
+  // Encode the header and payload as base64 strings
+  const base64Header = base64UrlEncode(JSON.stringify(header));
+  const base64Payload = base64UrlEncode(JSON.stringify(object));
+
+  // Create the signature
+  const signature = process.env.SECRET;
+
+  // Return the JWT
+  return `${base64Header}.${base64Payload}.${signature}`;
+}
 
 export function hash_password(password){
      const hasPassword=bcrypt.hash(password,process.env.SECRET);
