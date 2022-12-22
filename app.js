e941495148162b2197f703235f2b9bc5dc37680f
Rishi <rishichaturvedi0012@gmail.com>
2022-12-22 21:00:37 +0530

    added captcha verification in utils/recaptacha.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 import db from './config/db.config.js';
 db();
 
--- utils/recaptcha.js
+++ utils/recaptcha.js
@@ -1,1 +1,21 @@
-export default function verifycaptcha(token){return true;}
+import recaptchaVerify from recaptcha-verify 
+import { response_400 } from './responseCodes.js'
+
+export default function verifycaptcha(token){
+    var recaptcha = new recaptchaVerify({
+        secret: process.env.GOOGLE_RECAPTCHA_SECRET_KEY,
+        verbose: true
+      });
+    
+    recaptcha.checkResponse(token, function(error, response){
+        if(error){
+            response_400(res, error.toString)
+            return;
+        }
+        if(response.success){
+            return true
+        }else{
+            return false
+        }
+    });
+}
