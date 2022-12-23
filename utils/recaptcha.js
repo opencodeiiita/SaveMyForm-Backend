fd8a7dccf7f4c7aa67475dda671db77a67025be8
Rishi <rishichaturvedi0012@gmail.com>
2022-12-23 09:14:14 +0530

    made changes in recaptcha.js

--- utils/recaptcha.js
+++ utils/recaptcha.js
@@ -2,6 +2,8 @@ import recaptchaVerify from recaptcha-verify
 import { response_400 } from './responseCodes.js'
 
 export default function verifycaptcha(token){
+    if(process.env.ENV === 'dev') return true;
+
     var recaptcha = new recaptchaVerify({
         secret: process.env.GOOGLE_RECAPTCHA_SECRET_KEY,
         verbose: true
@@ -9,8 +11,8 @@ export default function verifycaptcha(token){
     
     recaptcha.checkResponse(token, function(error, response){
         if(error){
-            response_400(res, error.toString)
-            return;
+            console.log(err);
+            return false;
         }
         if(response.success){
             return true
