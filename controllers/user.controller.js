622d01333b600f3e1f6f360b0556a2a957d6eccd
Sanyam07 <agrawalsanyam07@gmail.com>
2022-12-27 18:49:53 +0530

    updateUser function added in userController.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { sendVerificationLink } from '../utils/mailer.js';
-import { getJwt } from '../utils/password.js';
+import { getJwt, hash_password } from '../utils/password.js';
 import { response_200,response_400 } from '../utils/responseCodes.js';
 
 export function getVerificationLink(req,res){
@@ -14,4 +14,28 @@ export function getVerificationLink(req,res){
     const token = getJwt(payload,'300s')
     sendVerificationLink(payload.email,token,payload.ip)
     return response_200(res,'Verification mail sent')
+}
+
+export async function updateUser(req,res){
+    if(!(req.body.name && req.body.email && req.body.oldPassword)) return response_400(res,'All required fields not present')
+    if(req.user.passwordHash!==''){
+        const password = await hash_password(req.body.oldPassword)
+        if(password!=req.user.passwordHash) return response_400(res,'Wrong Password')
+    }
+    if(req.body.newPassword && req.body.newPassword!==''){
+        if(req.body.newPassword.length<6)
+            return response_400(res,'Password length too short')
+        const password = await hash_password(req.body.newPassword)   
+        req.user.passwordHash = password
+    }
+    if(req.body.name!==req.user.name) req.user.name = req.body.name
+    if(req.body.email!==req.user.email){
+        req.user.email = req.body.email
+        req.user.verified = false
+    }
+    const updatedUser = await req.user.save()
+    return response_200(res,'User info updated',{
+        name:updatedUser.name,
+        email:updatedUser.email
+    })
 }
