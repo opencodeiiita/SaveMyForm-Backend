82ef4173cdc9e0a0bdbbe1d4922a320284ef2de1
Sanyam07 <agrawalsanyam07@gmail.com>
2022-12-27 12:58:43 +0530

    getVerificationLink function added in userController

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,9 @@
 import { sendVerificationLink } from '../utils/mailer.js';
 import { getJwt } from '../utils/password.js';
-import { response_200 } from '../utils/responseCodes.js';
+import { response_200,response_400 } from '../utils/responseCodes.js';
 
 export function getVerificationLink(req,res){
-    if(req.user.verified) return;
+    if(req.user.verified) return response_400(res,'The user is already verified')
     const payload = {
         name : req.user.name,
         email : req.user.email,
