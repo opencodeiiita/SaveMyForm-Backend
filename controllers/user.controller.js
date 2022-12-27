de7c1cef4b7bd78bcc58f9da249e2ddecb49ce70
Sanyam07 <agrawalsanyam07@gmail.com>
2022-12-27 23:23:28 +0530

    updateUser function added in userController.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { sendVerificationLink } from '../utils/mailer.js';
 import { getJwt, hash_password } from '../utils/password.js';
 import { response_200,response_400 } from '../utils/responseCodes.js';
+import verifycaptcha from '../utils/recaptcha.js';
 import validator from 'validator';
 
 export function getVerificationLink(req,res){
@@ -20,6 +21,7 @@ export function getVerificationLink(req,res){
 export async function updateUser(req,res){
     if(!(req.body.name && req.body.email && req.body.password)) return response_400(res,'All required fields not present')
     if(req.user.passwordHash!==''){
+        if(!verifycaptcha(req.body.recaptcha_token)) return response_400(res,'Captcha not verified')
         const password = await hash_password(req.body.password)
         if(password!==req.user.passwordHash) return response_400(res,'Wrong Password')
         if(req.body.email!==req.user.email){
