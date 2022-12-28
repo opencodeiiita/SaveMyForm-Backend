18e0750be0d821e5e95e05792ef7c4533b4398cd
Pranav TIwari <pranavt150@gmail.com>
2022-12-28 17:52:17 +0530

    added /user/dashboard get request

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,58 +1,87 @@
 import { sendVerificationLink } from '../utils/mailer.js';
 import { getJwt, hash_password } from '../utils/password.js';
-import { response_200,response_400 } from '../utils/responseCodes.js';
+import { response_200, response_400 } from '../utils/responseCodes.js';
 import verifycaptcha from '../utils/recaptcha.js';
 import validator from 'validator';
 
-export function getVerificationLink(req,res){
-    if(req.user.verified) return response_400(res,'The user is already verified')
-    const payload = {
-        name : req.user.name,
-        email : req.user.email,
-        id : req.user._id,
-        ip : req.ip
-    }
+export function getVerificationLink(req, res) {
+  if (req.user.verified)
+    return response_400(res, 'The user is already verified');
+  const payload = {
+    name: req.user.name,
+    email: req.user.email,
+    id: req.user._id,
+    ip: req.ip,
+  };
 
-    const token = getJwt(payload,'300s')
-    sendVerificationLink(payload.email,token,payload.ip)
-    return response_200(res,'Verification mail sent')
+  const token = getJwt(payload, '300s');
+  sendVerificationLink(payload.email, token, payload.ip);
+  return response_200(res, 'Verification mail sent');
 }
 
-export async function updateUser(req,res){
-    if(!(req.body.name && req.body.email && req.body.password)) return response_400(res,'All required fields not present')
-    if(req.user.passwordHash!==''){
-        if(!verifycaptcha(req.body.recaptcha_token)) return response_400(res,'Captcha not verified')
-        const password = await hash_password(req.body.password)
-        if(password!==req.user.passwordHash) return response_400(res,'Wrong Password')
-        if(req.body.email!==req.user.email){
-            if(!validator.isEmail(req.body.email)) return response_400(res,'Invalid email id')
-            req.user.email = req.body.email
-            req.user.verified = false
-        }
-        if(req.body.name!==req.user.name) req.user.name = req.body.name
-        const updatedUser = await req.user.save()
-        return response_200(res,'User info updated',{
-            name:updatedUser.name,
-            email:updatedUser.email
-        })
+export async function updateUser(req, res) {
+  if (!(req.body.name && req.body.email && req.body.password))
+    return response_400(res, 'All required fields not present');
+  if (req.user.passwordHash !== '') {
+    if (!verifycaptcha(req.body.recaptcha_token))
+      return response_400(res, 'Captcha not verified');
+    const password = await hash_password(req.body.password);
+    if (password !== req.user.passwordHash)
+      return response_400(res, 'Wrong Password');
+    if (req.body.email !== req.user.email) {
+      if (!validator.isEmail(req.body.email))
+        return response_400(res, 'Invalid email id');
+      req.user.email = req.body.email;
+      req.user.verified = false;
     }
-    else{
-        return response_400(res,'You will need to create a password before changing these settings')
-    }
+    if (req.body.name !== req.user.name) req.user.name = req.body.name;
+    const updatedUser = await req.user.save();
+    return response_200(res, 'User info updated', {
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    return response_400(
+      res,
+      'You will need to create a password before changing these settings',
+    );
+  }
 }
 
-export async function updatePassword(req,res){
-    if(!(req.body.oldPassword && req.body.newPassword)) return response_400(res,'All request parameters not provided')
-    if(!verifycaptcha(req.body.recaptcha_token)) return response_400(res,'Captcha not verified')
-    if(req.user.passwordHash!==''){
-        const password = await hash_password(req.body.oldPassword)
-        if(password!==req.user.passwordHash) return response_400(res,'Wrong Password')
-        if(req.body.newPassword.length<6) return response_400(res,'New password length too short')
-        req.user.passwordHash = await hash_password(req.body.newPassword)
-        await req.user.save();
-        return response_200(res,'Password updated')
-    }
-    else{
-        return response_400(res,'You will need to create a password before changing these settings')
-    }
+export async function updatePassword(req, res) {
+  if (!(req.body.oldPassword && req.body.newPassword))
+    return response_400(res, 'All request parameters not provided');
+  if (!verifycaptcha(req.body.recaptcha_token))
+    return response_400(res, 'Captcha not verified');
+  if (req.user.passwordHash !== '') {
+    const password = await hash_password(req.body.oldPassword);
+    if (password !== req.user.passwordHash)
+      return response_400(res, 'Wrong Password');
+    if (req.body.newPassword.length < 6)
+      return response_400(res, 'New password length too short');
+    req.user.passwordHash = await hash_password(req.body.newPassword);
+    await req.user.save();
+    return response_200(res, 'Password updated');
+  } else {
+    return response_400(
+      res,
+      'You will need to create a password before changing these settings',
+    );
+  }
+}
+
+export async function dashboard(req, res) {
+  if (req.isAuthenticated) {
+    return response_200(
+      res,
+      'Authorization verified, You can see the dashboard now.',
+      {
+        name: req.user.name,
+        email: req.user.email,
+        verified: req.user.verified,
+        project_count: req.user.projects.length(),
+        projects: req.user.projects,
+      },
+    );
+  }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,12 @@
 import { greet } from '../controllers/auth.controller.js';
 
 import { Router } from 'express';
+import authMiddleware from '../middlewares/auth.middleware.js';
+import { dashboard } from '../controllers/user.controller.js';
 const router = Router();
 
 // All routes configured here
 router.get('/', greet);
+router.get('/dashboard', authMiddleware , dashboard);
 
 export default router;
