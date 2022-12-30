889db1dbf6c7e528595e6ef226e0eddbab57ee07
Pranav TIwari <pranavt150@gmail.com>
2022-12-31 01:15:20 +0530

    added routes for user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,27 @@
 // import handler (controller) functions to route them
 import { greet } from '../controllers/auth.controller.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
+import verifyMiddleware from '../middlewares/verify.middleware.js';
+import {
+  getVerificationLink,
+  updateUser,
+  updatePassword,
+  verifySecret,
+  getUser,
+  dashboard,
+} from '../controllers/user.controller.js';
 
 import { Router } from 'express';
+
 const router = Router();
 
 // All routes configured here
 router.get('/', greet);
+router.get('/raiseverification', authMiddleware, getVerificationLink);
+router.get('/verify/:secret', verifySecret);
+router.patch('/update', verifyMiddleware, updateUser);
+router.patch('/updatepassword', authMiddleware, updatePassword);
+router.get('/dashboard', verifyMiddleware, dashboard);
+router.get('/self', authMiddleware, getUser);
 
 export default router;
