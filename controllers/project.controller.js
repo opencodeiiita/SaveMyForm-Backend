17801edcb2915650019da66115e23eb5b5b19984
Madhur Gupta <76112446+Im-Madhur-Gupta@users.noreply.github.com>
2023-01-02 22:24:26 +0530

    Add update project controller (#82)
    * Add update project controller
    
    * Add mongoose query string to build res data in update project controller
    
    * Add populate calls in update project controller

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -1,9 +1,11 @@
 import Project from '../models/project.model.js';
+import User from "../models/user.model";
 import verifycaptcha from '../utils/recaptcha.js';
 import {
   response_200,
   response_201,
   response_400,
+  response_401,
   response_500,
 } from '../utils/responseCodes.js';
 
@@ -45,6 +47,60 @@ export async function deleteProject(req, res) {
     return response_400(res, 'Wrong Password');
   Project.deleteOne({ id: project.id });
   response_200(res, 'The project has been succesfully deleted.');
+}
+
+export async function updateProject(req, res) {
+  try {
+    if (!verifycaptcha(req.body.recaptcha_token)) {
+      return response_400(res, 'Captcha not verified');
+    }
+
+    const projectId = req.params.projectId;
+    const { ownerId } = await Project.findById(projectId, "-_id owner");
+
+    if (req.user.id !== ownerId) {
+      // current user is not the owner of the project
+      response_401('The user is not the owner of project.')
+    }
+
+    const password = await hash_password(req.body.password);
+    if (password !== req.user.passwordHash) {
+      return response_400(res, 'Wrong Password');
+    }
+
+    // creating the updatedProject object
+    const updatedProject = {};
+    if (req.body.name) {
+      updatedProject.name = req.body.name;
+    }
+    if (req.body.hasRecaptcha) {
+      updatedProject.allowRecaptcha = req.body.hasRecaptcha;
+    }
+    if (req.body.reCaptchaKey) {
+      updatedProject.reCaptchaKey = req.body.reCaptchaKey;
+    }
+    if (req.body.reCaptchaSecret) {
+      updatedProject.reCaptchaSecret = req.body.reCaptchaSecret;
+    }
+    if (req.body.allowedOrigins) {
+      updatedProject.allowedOrigins = req.body.allowedOrigins;
+    }
+    if (req.body.collaborators) {
+      updatedProject.collaborators = req.body.collaborators;
+    }
+
+    // updating the project details in DB
+    const finalProject = await Project.findByIdAndUpdate(projectId, updatedProject, { returnDocument: "after", select: "-_id -forms" })
+      .populate({ path: "owner", select: "-_id name email" })
+      .populate({ path: "collaborators", select: "-_id name email" });
+
+    finalProject.is_owner = req.user.id === finalProject.owner;
+
+    response_200(res, "Project updated", finalProject);
+  }
+  catch (err) {
+    response_500("Something went wrong, please try again.");
+  }
 }
 
 export async function projectDashboard(req,res){
