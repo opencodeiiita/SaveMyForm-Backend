b534a7d6ab22d27d113a74c3095402a0077947f3
Anurag Jain <76420937+BuddyLongLegs@users.noreply.github.com>
2023-01-24 02:53:03 +0530

    minor bugs fixed (#111)

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use('/user', userRoutes);
 
 app.listen(
   process.env.PORT ? process.env.PORT : 8080,
+  process.env.HOST ? process.env.HOST : '127.0.0.1',
   console.log(
     `listening on http://localhost:${
       process.env.PORT ? process.env.PORT : 8080
