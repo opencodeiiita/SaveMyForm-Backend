0ed86409ea78367754f2b691f407bfbdb84183e3
chandra <chandra091827@gamil.com>
2023-06-02 22:17:48 +0530

    added routes to update collaborator

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,6 +1,6 @@
 // import handler (controller) functions to route them
 import { greet } from '../controllers/auth.controller.js';
-import { createProject, deleteProject, projectDashboard, updateProject ,AddCollaborator,removeCollaborator} from '../controllers/project.controller.js';
+import { createProject, deleteProject, projectDashboard, updateProject ,UpdateCollaborator} from '../controllers/project.controller.js';
 import verifiedMiddleware from '../middlewares/verify.middleware.js';
 
 import { Router } from 'express';
@@ -12,7 +12,6 @@ router.post("/new", verifiedMiddleware, createProject);
 router.get("/dashboard/:id", verifiedMiddleware, projectDashboard);
 router.patch("/update/:projectId", verifiedMiddleware, updateProject);
 router.delete("/delete/:id", verifiedMiddleware, deleteProject)
-router.post("/addcollaborator", AddCollaborator)
-router.post("/removecollaborator", removeCollaborator)
+router.post("/UpdateCollaborator", UpdateCollaborator)
 
 export default router;
